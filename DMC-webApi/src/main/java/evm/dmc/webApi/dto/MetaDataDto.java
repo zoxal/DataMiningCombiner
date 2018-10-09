206bf7d056ec1e616d1ba05bed51108f70ea220f
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-09 19:38:50 +0300

    REST-1 - Document MetaDataDto.java

--- DMC-webApi/src/main/java/evm/dmc/webApi/dto/MetaDataDto.java
+++ DMC-webApi/src/main/java/evm/dmc/webApi/dto/MetaDataDto.java
@@ -7,9 +7,15 @@ import org.springframework.hateoas.ResourceSupport;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * DTO for MetaData model
+ * 
+ * @see evm.dmc.api.model.data.MetaData
+ */
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class MetaDataDto  extends ResourceSupport {
+
 	private Long metaDataId;
 	
 	private String name;
