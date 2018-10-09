9022248b2582e04349d33017c83973770208cd3b
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-09 19:40:34 +0300

    REST-1 - Document ProjectDto.java

--- DMCweb/src/main/java/evm/dmc/rest/dto/ProjectDto.java
+++ DMCweb/src/main/java/evm/dmc/rest/dto/ProjectDto.java
@@ -1,19 +1,22 @@
 package evm.dmc.rest.dto;
 
 import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.Date;
 
 import org.springframework.hateoas.ResourceSupport;
 
 import evm.dmc.api.model.ProjectType;
-import evm.dmc.api.model.account.Role;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * DTO for Project model
+ *
+ * @see evm.dmc.api.model.ProjectModel
+ */
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class ProjectDto extends ResourceSupport {
+
 	private Long projectId;
 	
 	private ProjectType projectType;
