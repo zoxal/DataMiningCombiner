2bc2b858884e425afdeb1c60db94b4e95cdf1542
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-09 19:33:21 +0300

    REST-1 - Document FrameworkNotFoundException.java

--- DMC-webApi/src/main/java/evm/dmc/webApi/exceptions/FrameworkNotFoundException.java
+++ DMC-webApi/src/main/java/evm/dmc/webApi/exceptions/FrameworkNotFoundException.java
@@ -1,10 +1,11 @@
 package evm.dmc.webApi.exceptions;
 
+/**
+ * Throwed when no Framework found
+ */
 public class FrameworkNotFoundException extends NotFoundException {
 
-	/**
-	 * 
-	 */
+	/** Defined to avoid problems with serialization */
 	private static final long serialVersionUID = 8935412243515578391L;
 
 	public FrameworkNotFoundException(String message) {
