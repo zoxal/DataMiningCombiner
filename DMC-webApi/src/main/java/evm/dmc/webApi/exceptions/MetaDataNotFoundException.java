791be3b155c051951d0bfd8a77bf725a9d08c3cc
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-09 19:29:01 +0300

    REST-1 - Document MetaDataNotFountException.java

--- DMC-webApi/src/main/java/evm/dmc/webApi/exceptions/MetaDataNotFoundException.java
+++ DMC-webApi/src/main/java/evm/dmc/webApi/exceptions/MetaDataNotFoundException.java
@@ -1,10 +1,11 @@
 package evm.dmc.webApi.exceptions;
 
+/**
+ * Throwed when no MetaData found
+ */
 public class MetaDataNotFoundException extends NotFoundException {
-	
-	/**
-	 * 
-	 */
+
+	/** Defined to avoid problems with serialization */
 	private static final long serialVersionUID = -7557190537228887032L;
 
 	public MetaDataNotFoundException(String message) {
