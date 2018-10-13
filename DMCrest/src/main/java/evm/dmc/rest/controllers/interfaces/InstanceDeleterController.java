7c51c21f2efa5b25bf408f6564baff5ceab3627b
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-13 21:39:32 +0300

    REST-1 - Document interface InstanceDeleterController.java

--- DMCrest/src/main/java/evm/dmc/rest/controllers/interfaces/InstanceDeleterController.java
+++ DMCrest/src/main/java/evm/dmc/rest/controllers/interfaces/InstanceDeleterController.java
@@ -2,7 +2,25 @@ package evm.dmc.rest.controllers.interfaces;
 
 import evm.dmc.webApi.dto.AbstractDto;
 
+/**
+ * defines HATEOAS REST API interface for deleting of instances
+ * @param <T> - DTO
+ */
 public interface InstanceDeleterController <T extends AbstractDto> {
+
+	/**
+	 * deletes existing instance
+	 * @param accountId Account model identifier
+	 * @param id identifier of instance for deleting
+	 * @return deleted DTO
+	 */
 	T deleteInstance(Long accountId, Long id);
+
+	/**
+	 * deletes existing instance
+	 * @param accountId Account model identifier
+	 * @param dto DTO of instance for deleting
+	 * @return deleted DTO
+	 */
 	T deleteInstance(Long accountId, T dto);
 }
