c50dd8a26d2b8332b6daa818e1af912599bb3e70
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-13 21:39:10 +0300

    REST-1 - Document interface InstanceAdderController.java

--- DMCrest/src/main/java/evm/dmc/rest/controllers/interfaces/InstanceAdderController.java
+++ DMCrest/src/main/java/evm/dmc/rest/controllers/interfaces/InstanceAdderController.java
@@ -2,8 +2,25 @@ package evm.dmc.rest.controllers.interfaces;
 
 import evm.dmc.webApi.dto.AbstractDto;
 
+/**
+ * defines HATEOAS REST API interface for getting instances
+ * @param <T> - DTO
+ */
 public interface InstanceAdderController<T extends AbstractDto> {
-	T postNewInstance(Long accountId, T dto);
 
-	T psotUpdateInstance(Long accountId, T dto);
+	/**
+	 * adds new instance
+	 * @param accountId Account model identifier
+	 * @param dto DTO for adding
+	 * @return added DTO
+	 */
+	T addInstance(Long accountId, T dto);
+
+	/**
+	 * updates existing instance
+	 * @param accountId Account model identifier
+	 * @param dto DTO with updated information
+	 * @return updated DTO
+	 */
+	T updateInstance(Long accountId, T dto);
 }
